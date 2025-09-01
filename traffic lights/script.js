2918c53de314f1bdedcd43a722d2a6d2fabd7fdf
Mounika-Thirupathi <thirupathim848@gmail.com>
2025-09-01 16:39:41 +0530

    intial msg

--- traffic lights/script.js
+++ traffic lights/script.js
@@ -1,22 +1,22 @@
-document.body.style.backgroundColor="brown"
-function red(){
-    document.body.style.backgroundColor="red"
-    document.getElementById("color").textContent="stop"
-    
-
-}
+document.body.style.backgroundColor="darkgray"
 
 function green(){
     document.body.style.backgroundColor="green"
-    document.getElementById("color").textContent="start"
-
+    document.getElementById("color").textContent="Go 🚦";
 }
 
 
 function orange(){
     document.body.style.backgroundColor="orange"
-    document.getElementById("color").textContent="ready"
+    document.getElementById("color").textContent="Get Ready ⚠️";
     
 
+}
+
+
+function red(){
+    document.body.style.backgroundColor="red"
+    document.getElementById("color").textContent="Stop ⛔";
+    
 
 }
